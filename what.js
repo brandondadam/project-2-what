66571ee1a6b2defbc46243cabf9ee0047e1991e7
bdadam <bdadam@mail.bradley.edu>
2015-10-04 04:06:11 -0500

    first breakthrough with a button

--- what.js
+++ what.js
@@ -1,6 +1,17 @@
 var textarea = document.getElementById('what');
 var form = document.getElementsByTagName('form')[0];
 var what = location.search;
+var submit_btn = document.getElementById('submit');
+var answer = document.getElementById('answer');
+
+var right = "Yup, that car is pretty nice.";
+var wrong = "I think that car sucks.";
+
+
+submit_btn.addEventListener('click', function(){
+	answer.innerHTML = right;
+	answer.innerHTML = wrong;
+});
 
 textarea.addEventListener('change', function(){
 	form.submit();
