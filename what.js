72f468cc0f69f35f1e66c48284b38e8764566bf0
bdadam <bdadam@mail.bradley.edu>
2015-10-04 23:03:36 -0500

    added pictures that work, and is fully functioning now

--- what.js
+++ what.js
@@ -50,62 +50,62 @@ for(var i = 0; i < 25; i++){
 	}
 }
 var div = document.getElementById('car');
- submit_btn.addEventListener('click', function() {
-	 if (what == correctAnswers[0]){
-		 div.innerHTML = '<img src="rx7.jpg">';
-	 }
-	 if (what == correctAnswers[1]){
-		 div.innerHTML = '<img src="rx8.jpg">';
-	 }
-	 if (what == correctAnswers[2]){
-		 div.innerHTML = '<img src="rx3.jpg">';
-	 }
-	 if (what == correctAnswers[3] || what == correctAnswers[4]){
-		 div.innerHTML = '<img src="miata.jpg">';
-	 }
-	 if (what == correctAnswers[5]){
-		 div.innerHTML = '<img src="s15.jpg">';
-	 }
-	 if (what == correctAnswers[6]){
-		 div.innerHTML = '<img src="240.jpg">';
-	 }
-	 if (what == correctAnswers[7]){
-		 div.innerHTML = '<img src="300zx.jpg">';
-	 }
-	 if (what == correctAnswers[8]){
-		 div.innerHTML = '<img src="fairlady.jpg">';
-	 }
-	 if (what == correctAnswers[9] || what == correctAnswers[10] || what == correctAnswers[11]){
-		 div.innerHTML = '<img src="240z.jpg">';
-	 }
-	 if (what == correctAnswers[12]){
-		 div.innerHTML = '<img src="celica.jpg">';
-	 }
-	 if (what == correctAnswers[13]){
-		 div.innerHTML = '<img src="celicaSupra.jpg">';
-	 }
-	 if (what == correctAnswers[14]){
-		 div.innerHTML = '<img src="mkiii.jpg">';
-	 }
-	 if (what == correctAnswers[15]){
-		 div.innerHTML = '<img src="corolla.jpg">';
-	 }
-	 if (what == correctAnswers[16]){
-		 div.innerHTML = '<img src="mr2.jpg">';
-	 }
-	 if (what == correctAnswers[17]){
-		 div.innerHTML = '<img src="s2k.jpg">';
-	 }
-	 if (what == correctAnswers[18] || what == correctAnswers[19]){
-		 div.innerHTML = '<img src="nsx.jpg">';
-	 }
-	 if (what == correctAnswers[20] || what == correctAnswers[21]){
-		 div.innerHTML = '<img src="sti.jpg">';
-	 }
-	 if (what == correctAnswers[22] || what == correctAnswers[23]){
-		 div.innerHTML = '<img src="evo.jpg">';
-	 }
- }, false);
+submit_btn.addEventListener('click', function() {
+	if (what == correctAnswers[0]){
+		div.innerHTML = '<img src="rx7.jpg">';
+	}
+	if (what == correctAnswers[1]){
+		div.innerHTML = '<img src="rx8.jpg">';
+	}
+	if (what == correctAnswers[2]){
+		div.innerHTML = '<img src="rx3.jpg">';
+	}
+	if (what == correctAnswers[3] || what == correctAnswers[4]){
+		div.innerHTML = '<img src="miata.jpg">';
+	}
+	if (what == correctAnswers[5]){
+		div.innerHTML = '<img src="s15.jpg">';
+	}
+	if (what == correctAnswers[6]){
+		div.innerHTML = '<img src="240.jpg">';
+	}
+	if (what == correctAnswers[7]){
+		div.innerHTML = '<img src="300zx.jpg">';
+	}
+	if (what == correctAnswers[8]){
+		div.innerHTML = '<img src="fairlady.jpg">';
+	}
+	if (what == correctAnswers[9] || what == correctAnswers[10] || what == correctAnswers[11]){
+		div.innerHTML = '<img src="240z.jpg">';
+	}
+	if (what == correctAnswers[12]){
+		div.innerHTML = '<img src="celica.jpg">';
+	}
+	if (what == correctAnswers[13]){
+		div.innerHTML = '<img src="celicaSupra.jpg">';
+	}
+	if (what == correctAnswers[14]){
+		div.innerHTML = '<img src="mkiii.jpg">';
+	}
+	if (what == correctAnswers[15]){
+		div.innerHTML = '<img src="corolla.jpg">';
+	}
+	if (what == correctAnswers[16]){
+		div.innerHTML = '<img src="mr2.jpg">';
+	}
+	if (what == correctAnswers[17]){
+		div.innerHTML = '<img src="s2k.jpg">';
+	}
+	if (what == correctAnswers[18] || what == correctAnswers[19]){
+		div.innerHTML = '<img src="nsx.jpg">';
+	}
+	if (what == correctAnswers[20] || what == correctAnswers[21]){
+		div.innerHTML = '<img src="sti.jpg">';
+	}
+	if (what == correctAnswers[22] || what == correctAnswers[23]){
+		div.innerHTML = '<img src="evo.jpg">';
+	}
+}, false);
 
 submit_btn.addEventListener('click', function(){
 	if(correct == true){
